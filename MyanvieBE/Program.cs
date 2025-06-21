64fd7516de759b92c0a1441915384c904589a8d5
Mycroft <mycroft180202@gmail.com>
2025-06-21 16:33:21 +0700

    Update Program.cs

--- MyanvieBE/Program.cs
+++ MyanvieBE/Program.cs
@@ -86,7 +86,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:3000")
+                          policy.WithOrigins("https://myanvie.netlify.app")
                                 .AllowAnyHeader()
                                 .AllowAnyMethod();
                       });
