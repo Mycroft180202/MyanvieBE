c3088e7289d798cfc8b942dc2df0556302742269
Mycroft <mycroft180202@gmail.com>
2025-06-26 14:54:09 +0700

    fix

--- MyanvieBE/Migrations/20250626075350_FixPendingModelChangesAfterDeploy.Designer.cs
+++ MyanvieBE/Migrations/20250626075350_FixPendingModelChangesAfterDeploy.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyanvieBE.Data;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MyanvieBE.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250626075350_FixPendingModelChangesAfterDeploy")]
+    partial class FixPendingModelChangesAfterDeploy
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MyanvieBE/Migrations/20250626075350_FixPendingModelChangesAfterDeploy.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyanvieBE.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPendingModelChangesAfterDeploy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Orders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Orders",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
